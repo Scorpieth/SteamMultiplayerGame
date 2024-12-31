dbc17ae76223174ebff66269ec46fe1a27de9cde
Scorpie <scorpieth@gmail.com>
2024-12-31 15:41:06 +0100

    Reinstall addons for Steam Move some files to its proper directory Remove unused .cs files

--- world.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-class_name WorldState
-
-@export var playersContainer: Node
-
-# Called when the node enters the scene tree for the first time.
-func addPlayer(player: Node3D):
-	playersContainer.add_child(player)
-	pass
